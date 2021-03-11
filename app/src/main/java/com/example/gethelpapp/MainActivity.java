abf5804229acc8c70d98cbc43865a2aa9ec6db3c
c18301026 <c18301026@mytudublin.ie>
2021-03-11 19:34:12 +0000

    First commit

---
+++ app/src/main/java/com/example/gethelpapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.gethelpapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeActivity(View view) {
+        if(view.getId() == R.id.registerButton) {
+            Intent i = new Intent(this, MenuActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gethelpapp/PlannerActivity.java
@@ -0,0 +1,27 @@
+package com.example.gethelpapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class PlannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planner);
+    }
+
+    public void changeActivity(View view) {
+        if(view.getId() == R.id.messageButton) {
+            Intent msgI = new Intent(this, MessageActivity.class);
+            startActivity(msgI);
+        }
+        if(view.getId() == R.id.menuButton) {
+            Intent mnI = new Intent(this, MenuActivity.class);
+            startActivity(mnI);
+        }
+    }
+}
