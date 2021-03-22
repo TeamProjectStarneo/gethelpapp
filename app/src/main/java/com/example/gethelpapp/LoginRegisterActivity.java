6ceadb09a0d61d85a7ecd309a5791ad153de1328
c18301026 <c18301026@mytudublin.ie>
2021-03-22 17:49:42 +0000

    Updated the login/register activity XML

--- app/src/main/java/com/example/gethelpapp/LoginRegisterActivity.java
+++ app/src/main/java/com/example/gethelpapp/LoginRegisterActivity.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_or_register);
     }
 
     public void changeActivity(View view) {
-        if(view.getId() == R.id.registerButton) {
+        if(view.getId() == R.id.loginButton) {
             Intent i = new Intent(this, MenuActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
