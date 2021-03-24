ed708f7e2dac9b0fe1a2a477b52d85bd9e407a2b
c18301026 <c18301026@mytudublin.ie>
2021-03-24 19:25:13 +0000

    Created the login activity

--- app/src/main/java/com/example/gethelpapp/LoginActivity.java
+++ app/src/main/java/com/example/gethelpapp/LoginActivity.java
@@ -6,17 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginRegisterActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_or_register);
+        setContentView(R.layout.activity_login);
     }
 
     public void changeActivity(View view) {
         if(view.getId() == R.id.loginButton) {
-            Intent i = new Intent(this, LoginActivity.class);
+            Intent i = new Intent(this, MenuActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        } if(view.getId() == R.id.backButton) {
+            Intent i = new Intent(this, LoginRegisterActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
         }
