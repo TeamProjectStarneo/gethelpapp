86a02137d4d20b7935f29bb9beab8d74dc777b75
c18301026 <c18301026@mytudublin.ie>
2021-04-06 15:23:32 +0100

    Added basic AlertDialog functionality to the emergency button

--- app/src/main/java/com/example/gethelpapp/MenuActivity.java
+++ app/src/main/java/com/example/gethelpapp/MenuActivity.java
@@ -1,10 +1,14 @@
 package com.example.gethelpapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -25,5 +29,24 @@ public class MenuActivity extends AppCompatActivity {
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
         }
+    }
+
+    public void showEmergency(View v) {
+        AlertDialog.Builder emergencyAlert = new AlertDialog.Builder(MenuActivity.this);
+        emergencyAlert.setTitle("Emergency");
+        emergencyAlert.setMessage("Contact Garda and SJOG?");
+        emergencyAlert.setPositiveButton("Sure", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MenuActivity.this, "Done! Calling the Garda now to assist", Toast.LENGTH_LONG).show();
+            }
+        });
+        emergencyAlert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MenuActivity.this, "Ok buddy", Toast.LENGTH_LONG).show();
+            }
+        });
+        emergencyAlert.create().show();
     }
 }
