38103c4c84667f781e4b7478b3641bdc173296ca
Antony Morris <C18465716@mytudublin.ie>
2021-04-09 06:10:23 +0100

    Revert "RegisterActivity + RegisterActivity.java some code changes to LoginActivity.java"
    This reverts commit a66227a7

--- app/src/main/java/com/example/gethelpapp/LoginActivity.java
+++ app/src/main/java/com/example/gethelpapp/LoginActivity.java
@@ -5,27 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
-
-
-    Button loginButton;
-    EditText loginEmail,loginPassword;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        loginEmail = (EditText) findViewById(R.id.emailField);
-        loginPassword = (EditText) findViewById(R.id.passwordField);
-        loginButton = (Button) findViewById(R.id.loginButton);
-
-        loginButton.setOnClickListener(this);
-
-
     }
 
     public void changeActivity(View view) {
@@ -34,21 +20,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
         } if(view.getId() == R.id.backButton) {
-            Intent i = new Intent(this, LoginRegisterActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(i);
+            finish();
         }
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.loginButton:
-                verifyLogin();
-                break;
-        }
-    }
-
-    private void verifyLogin() {
     }
 }
