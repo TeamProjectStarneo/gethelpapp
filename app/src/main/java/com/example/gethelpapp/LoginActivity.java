0d7a41b13e67a62baca4a23c48b6cba3b306b12e
Antony Morris <C18465716@mytudublin.ie>
2021-04-10 04:33:18 +0100

    squash! RegisterActivity + RegisterActivity.java some code changes to LoginActivity.java

--- app/src/main/java/com/example/gethelpapp/LoginActivity.java
+++ app/src/main/java/com/example/gethelpapp/LoginActivity.java
@@ -1,19 +1,27 @@
 package com.example.gethelpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+import com.example.gethelpapp.db.data.UserDataBase;
+import com.example.gethelpapp.db.model.User;
+import com.example.gethelpapp.db.data.UserDao;
+
+public class LoginActivity extends AppCompatActivity {
 
 
     Button loginButton;
     EditText loginEmail,loginPassword;
-
+    UserDataBase dataBase;
+    UserDao db;
+    private UserDao userDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,32 +31,34 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         loginPassword = (EditText) findViewById(R.id.passwordField);
         loginButton = (Button) findViewById(R.id.loginButton);
 
-        loginButton.setOnClickListener(this);
+        dataBase = Room.databaseBuilder(this, UserDataBase.class, "mi-database.db")
+                .allowMainThreadQueries()
+                .build();
+        db = dataBase.getUserDao();
 
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString().trim();
+                String password = loginPassword.getText().toString().trim();
+
+                User user = db.getUser(email, password);
+                if (user != null) {
+                    Intent i = new Intent(LoginActivity.this, MenuActivity.class);
+                    i.putExtra("User", user);
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Unregistered user, or incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public void changeActivity(View view) {
-        if(view.getId() == R.id.loginButton) {
-            Intent i = new Intent(this, MenuActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(i);
-        } if(view.getId() == R.id.backButton) {
-            Intent i = new Intent(this, LoginRegisterActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(i);
-        }
-    }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.loginButton:
-                verifyLogin();
-                break;
-        }
-    }
 
-    private void verifyLogin() {
-    }
+
+
+
 }
--- app/src/main/java/com/example/gethelpapp/RegisterActivity.java
+++ app/src/main/java/com/example/gethelpapp/RegisterActivity.java
@@ -1,62 +1,63 @@
 package com.example.gethelpapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.example.gethelpapp.db.AppDatabase;
-import com.example.gethelpapp.db.UserAccount;
+import com.example.gethelpapp.db.data.UserDataBase;
+import com.example.gethelpapp.db.model.User;
+import com.example.gethelpapp.db.data.UserDao;
+public class RegisterActivity extends AppCompatActivity  {
 
-public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
-    private UserAccount user;
-    private AppDatabase appDatabase;
+
     Button registerButton;
-    EditText emailField, registerPassword,registerConfirmPassword;
+    EditText emailField, registerPassword, registerConfirmPassword;
+
+    private UserDao userDao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
         emailField = (EditText) findViewById(R.id.emailField);
         registerPassword = (EditText) findViewById(R.id.passwordField);
-        //registerConfirmPassword = (EditText) findViewById(R.id.confirmpasswordfield);
+        registerConfirmPassword = (EditText) findViewById(R.id.passwordField2);
 
         registerButton = (Button) findViewById(R.id.signUpButton);
 
-        registerButton.setOnClickListener(this);
 
-    }
+        userDao = Room.databaseBuilder(this, UserDataBase.class, "mi-database.db").allowMainThreadQueries()
+                .build().getUserDao();
 
-    public void changeActivity(View view) {
-        // if(view.getId() == R.id.registerButton) {
-        //     Intent i = new Intent(this, LoginActivity.class);
-        //   i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        //     startActivity(i);
-    }
-    // }
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //String userName = editTextUsername.getText().toString().trim();
+                String email = emailField.getText().toString().trim();
+                String password = registerPassword.getText().toString().trim();
+                String passwordConf = registerConfirmPassword.getText().toString().trim();
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.signUpButton:
-                postData();
-                break;
-        }
-    }
+                if (password.equals(passwordConf)) {
+                    User user = new User("Name", password, email);
+                    userDao.insert(user);
+                    Intent moveToLogin = new Intent(RegisterActivity.this, LoginRegisterActivity.class);
+                    startActivity(moveToLogin);
 
-    private void postData() {
-        UserAccount user = appDatabase.accountDAO().findByEmail(emailField.getText().toString().trim());
-        if (user != null && !user.getEmail().equalsIgnoreCase(emailField.getText().toString().trim()))
-        {
-            //post data
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Password is not matching", Toast.LENGTH_SHORT).show();
 
-        }
-        else{
-            //show alert "email already in use"
-        }
+                }
+            }
+        });
+
     }
 }
+
