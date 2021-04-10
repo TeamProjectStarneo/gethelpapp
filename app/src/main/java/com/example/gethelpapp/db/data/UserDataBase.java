651a5019243dc1f13e6d98af5db3bf3b4712e1a0
Antony Morris <C18465716@mytudublin.ie>
2021-04-10 04:33:29 +0100

    squash! RegisterActivity + RegisterActivity.java some code changes to LoginActivity.java

---
+++ app/src/main/java/com/example/gethelpapp/db/data/UserDataBase.java
@@ -0,0 +1,15 @@
+package com.example.gethelpapp.db.data;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.gethelpapp.db.model.User;
+
+
+@Database(entities = {User.class}, version = 1, exportSchema = false)
+public abstract class UserDataBase extends RoomDatabase {
+
+    public abstract UserDao getUserDao();
+
+}
+
