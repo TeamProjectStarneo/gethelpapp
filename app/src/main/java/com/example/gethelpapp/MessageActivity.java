095407c7229176fdc6c01a1f61cd5dd7d9e5f6ca
c18301026 <c18301026@mytudublin.ie>
2021-04-12 00:22:40 +0100

    Updated the message/inbox activity

--- app/src/main/java/com/example/gethelpapp/MessageActivity.java
+++ app/src/main/java/com/example/gethelpapp/MessageActivity.java
@@ -1,21 +1,35 @@
 package com.example.gethelpapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
-public class MessageActivity extends AppCompatActivity {
+public class MessageActivity extends ListActivity {
+    // Temporary
+    private String[] helpers = {"Dentist: Alice Miraci\nLast Message: Today at 1:00 PM", "Doctor: Dr Pepper\nLast Message: Yesterday at 2:00 AM"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        // Temporary
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, helpers));
     }
 
     public void changeActivity(View view) {
-        if(view.getId() == R.id.menuButton) {
+        if(view.getId() == R.id.profileButton) {
+            Intent i = new Intent(this, ProfileActivity.class);
+            startActivity(i);
+        }
+        if(view.getId() == R.id.settingsButton) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        }
+        if(view.getId() == R.id.menuButton || view.getId() == R.id.appHeader) {
             Intent i = new Intent(this, MenuActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
