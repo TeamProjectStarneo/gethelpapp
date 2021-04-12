ead4aad59472b252fe21e37458d06b30c417fdea
c18301026 <c18301026@mytudublin.ie>
2021-04-12 23:47:58 +0100

    Renamed planner XML to reminders XML & updated the reminders activity

--- app/src/main/java/com/example/gethelpapp/RemindersActivity.java
+++ app/src/main/java/com/example/gethelpapp/RemindersActivity.java
@@ -1,29 +1,50 @@
 package com.example.gethelpapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class RemindersActivity extends AppCompatActivity {
+public class RemindersActivity extends ListActivity {
+    // Temporary
+    private String[] helpers = {"Dentist Remove Cavity\nToday at 2:00 PM", "Doctor Appointment\nIn 4 days at 3:00 AM"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_planner);
+        setContentView(R.layout.activity_reminders);
+
+        // Temporary
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, helpers));
     }
 
     public void changeActivity(View view) {
+        if(view.getId() == R.id.profileButton) {
+            Intent i = new Intent(this, ProfileActivity.class);
+            startActivity(i);
+        }
+        if(view.getId() == R.id.settingsButton) {
+            Intent i = new Intent(this, SettingsActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(i);
+        }
         if(view.getId() == R.id.messageButton) {
             Intent i = new Intent(this, InboxActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
         }
-        if(view.getId() == R.id.menuButton) {
+        if(view.getId() == R.id.menuButton || view.getId() == R.id.appHeader) {
             Intent i = new Intent(this, MenuActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(i);
         }
+    }
+
+    // Temporary
+    protected void onListItemClick(ListView lv, View v, int pos, long id) {
+        // Intent i = new Intent(this, ReminderActivity.class);
+        // startActivity(i);
     }
 }
