29ba2de36878337bbe6edfbbe4da5f9ab9019c78
c18301026 <c18301026@mytudublin.ie>
2021-04-13 20:35:32 +0100

    Added a splash screen :D

---
+++ app/src/main/java/com/example/gethelpapp/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.gethelpapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch(Exception e) {
+                } finally {
+                    Intent i = new Intent(SplashActivity.this, LoginRegisterActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        t.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
